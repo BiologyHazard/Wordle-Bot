205ff165b7e6b8ff5484545323ea8d1999543d8d
BioHazard <3482991796@qq.com>
2022-03-18 08:21:42 +0800

    second ver.

--- wordle.py
+++ wordle.py
@@ -17,19 +17,6 @@ def num2list(hint_num):
         hint_num //= 3
     return list(reversed(ans))
 
-# def check(ans, guess, output):
-#     for i in range(5):
-#         if output[i] == '0':
-#             if guess[i] in ans:
-#                 return False
-#         elif output[i] == '1':
-#             if (not guess[i] in ans) or (guess[i] == ans[i]):
-#                 return False
-#         elif output[i] == '2':
-#             if not guess[i] == ans[i]:
-#                 return False
-#     return True
-
 
 def solve_hints():
     # wordle = Wordle()
@@ -51,7 +38,7 @@ def solve_hints():
         for guess in wordle.supported_guesses:
             lst.append(list2num(calc_hint(ans, guess)))
     with open(r"hints.json", 'w', encoding='utf-8') as f:
-        json.dump(hints, f, indent=None)
+        json.dump(hints, f, indent=None, separators=(',', ':'))
 
 
 def calc_hint(ans, guess):
@@ -76,49 +63,57 @@ def calc_hint(ans, guess):
 
 
 class Wordle:
+    with open(r"wordlist.json", 'r', encoding='utf-8') as f:
+        wordlist = json.load(f)
+    answers = wordlist['answer']
+    answers_reversed = {k: v for v, k in enumerate(answers)}
+    supported_guesses = wordlist['answer'] + wordlist['otherWord']
+    supported_guesses_reversed = {
+        k: v for v, k in enumerate(supported_guesses)}
+    with open(r"hints.json", 'r', encoding='utf-8') as f:
+        hint_list = json.load(f)
+
     def __init__(self):
-        with open(r"wordlist.json", 'r', encoding='utf-8') as f:
-            wordlist = json.load(f)
-        self.answers = wordlist['answer']
-        self.supported_guesses = wordlist['answer'] + wordlist['otherWord']
-        self.psb_answers = wordlist['answer']
-        with open(r"hints.json", 'r', encoding='utf-8') as f:
-            self.hint_list = json.load(f)
+        self.psb_answers = set(Wordle.answers)
+        self.len_psb_answers = len(self.psb_answers)
 
     def give_guess(self):
-        guess_list = {}
+        guess_list = []
         for guess_idx, guess in enumerate(self.supported_guesses):
-            hint_count = {}
-            for psb_ans_idx, psb_ans in enumerate(self.psb_answers):
-                try:
-                    hint_count[tuple(
-                        self.hint_list[psb_ans_idx][guess_idx])] += 1
-                except:
-                    hint_count[tuple(
-                        self.hint_list[psb_ans_idx][guess_idx])] = 1
+            hint_count = [0 for i in range(3 ** 5)]
+            for ans in self.psb_answers:
+                hint_count[self.hint_list[self.answers_reversed[ans]]
+                           [guess_idx]] += 1
             ent = 0
-            for hint, count in hint_count.items():
-                ent += count / len(self.psb_answers) * \
-                    (-math.log2(count / len(self.psb_answers)))
-            guess_list[guess_idx] = ent
+            for count in hint_count:
+                if count > 0:
+                    ent += (count / self.len_psb_answers *
+                            (-math.log2(count / self.len_psb_answers)))
+            guess_list.append(
+                ent + int(guess in self.psb_answers) / self.len_psb_answers)
         max_ent = -1
         max_guess = None
         for guess_idx, ent in enumerate(guess_list):
             if ent > max_ent:
                 max_ent = ent
-                max_guess = guess
+                max_guess = self.supported_guesses[guess_idx]
         print(max_guess, max_ent)
+        print(self.psb_answers)
+        self.give_guess()
 
     def store_result(self, guess, hint):
         # for psb_ans in self.possible_answers:
         #     if self.hint_dict[psb_ans][guess] != hint:
         #         self.possible_answers.remove(psb_ans)
-        self.psb_answers = [psb_ans for psb_ans in self.psb_answers
-                            if self.hint_list[psb_ans][guess] == hint]
+        self.psb_answers = {psb_ans for psb_ans in self.psb_answers
+                            if Wordle.hint_list[Wordle.answers_reversed[psb_ans]][Wordle.supported_guesses_reversed[guess]] == list2num(hint)}
+        self.len_psb_answers = len(self.psb_answers)
 
     def clear(self):
         self.psb_answers = self.answers
+        self.len_psb_answers = len(self.psb_answers)
 
 
 if __name__ == '__main__':
-    solve_hints()
+    # solve_hints()
+    pass
